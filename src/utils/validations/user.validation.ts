f7c34803e32ecee8fd317a7e604b908c35be77a8
Muhammad Ilyas <MuhammadIlyas523221@gmail.com>
2023-08-09 01:01:51 +0500

    refactor - set label to yup validation schema

--- src/utils/validations/user.validation.ts
+++ src/utils/validations/user.validation.ts
@@ -1,14 +1,14 @@
 import { object, string } from "yup";
 
 const userSchema = object().shape({
-  email: string().email("Email is invalid").required("Email is required"),
-  password: string().required("Password is required"),
-  name: string().required("Name is required"),
+  email: string().email().required().label("Email Address"),
+  password: string().required().label("Password"),
+  name: string().required().label("Name"),
 });
 
 const loginSchema = object().shape({
-  email: string().email("Email is invalid").required("Email is required"),
-  password: string().required("password is required field"),
+  email: string().email().required().label("Email Address"),
+  password: string().required().label("Password"),
 });
 
 export { userSchema, loginSchema };
