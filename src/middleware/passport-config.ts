e50cf4504374b850675fd9a5b190705a8c730a37
Muhammad Ilyas <MuhammadIlyas523221@gmail.com>
2023-08-09 12:06:05 +0500

    feat - add a passport jwt auth middleware

---
+++ src/middleware/passport-config.ts
@@ -0,0 +1,31 @@
+import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
+import { appEnvVars } from "@/utils/env-vars";
+import passport from "passport";
+import UserRepository from "@/db/repository/user-repo";
+
+const userRepo = new UserRepository();
+
+let jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: appEnvVars.jwtSecret,
+};
+
+const jwtStrategy = new JwtStrategy(jwtOptions, async (payload, done) => {
+  try {
+    const user = userRepo.findUser({ id: payload.id });
+
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  } catch (error) {
+    return done(error, false);
+  }
+});
+
+passport.use(jwtStrategy);
+
+export = passport.authenticate("jwt", {
+  session: false,
+});
